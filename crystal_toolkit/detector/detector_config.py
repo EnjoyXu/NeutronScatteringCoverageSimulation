f576aab1f5a5777d58f05db7a2eb8feec1bce355
EnjoyXu <806056453@qq.com>
2025-03-17 22:38:44 +0800

    add parameter phi_ranges_ to save the original phi for painting

--- crystal_toolkit/detector/detector_config.py
+++ crystal_toolkit/detector/detector_config.py
@@ -14,13 +14,13 @@ class DetectorConfig:
     # 晶体旋转的角度
     psi_range: Tuple[float, float]
     phi_ranges: List[Tuple[float, float]]
-    theta_ranges: List[Tuple[float, float]]
+    theta_ranges_: List[Tuple[float, float]]
 
     detector_w: Optional[np.ndarray] = None
 
     def __post_init__(self):
         # 将theta值从以u,v平面为0转化为一般的球坐标系中的theta值
-        self.theta_ranges = [sorted([90 - x[0], 90 - x[1]]) for x in self.theta_ranges]
+        self.theta_ranges = [sorted([90 - x[0], 90 - x[1]]) for x in self.theta_ranges_]
 
         # 由于定义的是晶体旋转的角度，但是之后的计算中，是转动入射方向的，所以相对的，这里需要将角度全部反向
         self.psi_range = sorted([-psi for psi in self.psi_range])
