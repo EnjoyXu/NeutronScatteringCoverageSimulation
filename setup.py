35c5f77aa4350071324550a5323b0ff5e5cd5f63
EnjoyXu <806056453@qq.com>
2025-07-18 18:19:53 +0800

    add requirements

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ setup(
     name="crystal_toolkit",
     version="0.0.1",
     packages=find_packages(),
-    install_requires=[],  # 依赖列表（如"requests>=2.25.1"）
+    install_requires=[
+        "numpy",
+        "plotly",
+    ],  # 依赖列表（如"requests>=2.25.1"）
     entry_points={"console_scripts": ["mycli=my_package.cli:main"]},  # 命令行工具配置
 )
