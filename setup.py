9cb79db347dac511c9b512501308ebb118f2b58b
EnjoyXu <806056453@qq.com>
2025-08-18 11:50:41 +0800

    add python version, packages

--- setup.py
+++ setup.py
@@ -2,11 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name="crystal_toolkit",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
+    python_requires=">=3.12",
     install_requires=[
         "numpy",
         "plotly",
+        "traitlets",
+        "matplotlib",
+        "dash",
+        "pymatgen",
     ],  # 依赖列表（如"requests>=2.25.1"）
     entry_points={"console_scripts": ["mycli=my_package.cli:main"]},  # 命令行工具配置
 )
