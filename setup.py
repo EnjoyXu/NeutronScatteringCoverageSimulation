265a6d855756a7f861973060931fa82e78d7d955
EnjoyXu <806056453@qq.com>
2025-10-09 11:33:40 +0800

    v0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="crystal_toolkit",
-    version="0.0.2",
+    version="0.0.3",
     packages=find_packages(),
     python_requires=">=3.12",
     install_requires=[
